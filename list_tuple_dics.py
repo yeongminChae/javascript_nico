7e44680cbef3373fca2dbaadbe49b07f9176fdb0
Yeongmin_Chae <codudals98@naver.com>
2022-06-04 15:57:35 +0900

    list, tuple, dictionary

---
+++ list_tuple_dics.py
@@ -0,0 +1,25 @@
+# list
+
+# days = ["mon", "tue", "wed", "thu", "fri", "sat"]
+# # print("mon" in days)
+# print(len(days))
+# days.append("sun")
+# print(len(days))
+
+# tuple
+
+# days = ("mon", "tue", "wed", "thu", "fri", "sat")
+# print(type(days))
+
+# dictionary
+
+nico = {
+    "name": "Nico",
+    "age": 29,
+    "korean": True,
+    "fav_food": ["kimchi", "water"]
+}
+# print(nico)
+# print(nico["fav_food"])
+nico["handsome"] = True
+print(nico)
