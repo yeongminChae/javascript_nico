8217cc7a85abe65879d3d80ad2b5c5ea7db17bcf
Yeongmin_Chae <codudals98@naver.com>
2022-06-04 21:53:04 +0900

    for loop

---
+++ for.py
@@ -0,0 +1,10 @@
+days = ("mon", "tue", "wed", "thu", "fri")
+for i in days:
+    print(i[0])
+print()
+days = ["mon", "tue", "wed", "thu", "fri"]
+for i in days:
+    if i is "wed":
+        break
+    else:
+        print(i)
