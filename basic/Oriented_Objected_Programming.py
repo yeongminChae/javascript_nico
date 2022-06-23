00c6869ac73fe520e5612851c79363a6bc7b83e5
Yeongmin_Chae <codudals98@naver.com>
2022-06-23 18:50:05 +0900

    OOP part2 inheritance in python

--- basic/Oriented_Objected_Programming.py
+++ basic/Oriented_Objected_Programming.py
@@ -45,7 +45,26 @@ class Car():
 
 porche = Car(color="Green", price="$50")
 # print(dir(Car)) # dir shows to us for all of the strings
-print(porche.color, porche.price)
+# print(porche.color, porche.price)
 
 mini = Car()
-print(mini.color, mini.price)
+# print(mini.color, mini.price)
+
+# inheritance : extends your code
+
+
+class Convertible(Car):  # how we extend the class
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)  # super is function which is call the parent's class
+        self.time = kwargs.get("time", 10)
+
+    def take_off(self):
+        return "taking off"
+
+    def __str__(self):
+        return f"Car without roof"
+
+
+ferrari = Convertible(color="Green", price="$50")
+print(ferrari.color, ferrari.price, ferrari.take_off)
